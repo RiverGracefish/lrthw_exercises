e3f10a74cad3853c682ea3984fddc097cdeabad5
Grace Yu <grace.yu4444@gmail.com>
2015-02-13 15:52:33 -0700

    update chapter 7 with comments

--- chapter_7/ex7_1.rb
+++ chapter_7/ex7_1.rb
@@ -1,21 +1,39 @@
+# print out Mary had a little lamb.
 puts 'Mary had a little lamb.'
+# print out Its fleece was white as snow.
 puts 'Its fleece was white as snow.'
+# print out And everywhere that Mary went.
 puts 'And everywhere that Mary went.'
+# print out 10 "."
 puts '.' * 10 # what'd that do?
 
+# assign end1 to C
 end1 = 'C'
+# assign end2 to h
 end2 = 'h'
+# assign end3 to e
 end3 = 'e'
+# assign end4 to e
 end4 = 'e'
+# assign end5 to s
 end5 = 's'
+# assign end6 to e
 end6 = 'e'
+# assign end7 to B
 end7 = 'B'
+# assign end8 to u
 end8 = 'u'
+# assign end9 to r
 end9 = 'r'
+# assign end10 to g
 end10 = 'g'
+# assign end11 to e
 end11 = 'e'
+# assign end12 to r
 end12 = 'r'
 
 # watch that print vs. puts on this line what's it do?
+# print out end1 + end2 + end3 + end4 + end5 + end6
 print end1 + end2 + end3 + end4 + end5 + end6
+# print out end7 + end8 + end9 + end10 + end11 + end12
 puts end7 + end8 + end9 + end10 + end11 + end12
