a3c515978803c9150e57acbac68f4e3d5155277b
Grace Yu <grace.yu4444@gmail.com>
2015-02-13 18:12:58 -0700

    update comments on chapter 10

--- chapter_10/ex10_1.rb
+++ chapter_10/ex10_1.rb
@@ -1,7 +1,14 @@
+# assign tabby_cat to I'm tabbed in. with Horizontal Tab
 tabby_cat = "\tI'm tabbed in."
+# assign persian_cat to I'm split linefeed on a line.
 persian_cat = "I'm split\non a line."
+# assign backslash_cat to I'm Backslash a Backslash cat.
 backslash_cat = "I'm \\ a \\ cat."
 
+# assign fat_cat to I'll do a list:
+# with Horizontal Tab * Cat food
+# with Horizontal Tab * Fishies
+# with Horizontal Tab * Catnip linefeed with Horizontal Tap * Grass
 fat_cat = "
 I'll do a list:
 \t* Cat food
@@ -9,7 +16,11 @@ I'll do a list:
 \t* Catnip\n\t* Grass
 "
 
+# print out tabby_cat
 puts tabby_cat
+# print out persian_cat
 puts persian_cat
+# print out backslash_cat
 puts backslash_cat
+# print out fat_cat
 puts fat_cat
