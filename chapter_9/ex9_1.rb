a57a6b9c96984dcaf7386d36cfa61b7e45e2dc63
Grace Yu <grace.yu4444@gmail.com>
2015-02-13 18:18:06 -0700

    update comments on chapter 9

--- chapter_9/ex9_1.rb
+++ chapter_9/ex9_1.rb
@@ -1,11 +1,20 @@
 # Here's some new strange stuff, remember type it exactly.
 
+# assign days to 'Mon Tue Wed Thu Fri Sat Sun'
 days = 'Mon Tue Wed Thu Fri Sat Sun'
+# assign months to "Jan linefeed Feb linefeed Mar linefeed Apr
+# linefeed May linefeed Jun linefeed Jul linefeed Aug"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# print out "Here are the days: #{days}"
 puts "Here are the days: #{days}"
+# print out "Here are the months: #{months}"
 puts "Here are the months: #{months}"
 
+# print out "There's something going on here.
+# With the three double-quotes.
+# We'll be able to type as much as we like.
+# Even 4 lines if we want, or 5, or 6."
 puts "
 There's something going on here.
 With the three double-quotes.
